37165a43db753bf9df0ed9b7121dea110ef5bc5c
Myner Soft <mynersoft@gmail.com>
2025-12-13 05:23:53 +0600

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,6 +11,10 @@ export default function RootLayout({ children }) {
 const [activeTab, setActiveTab] = useState('Home')
 	return (
 		<html lang="en">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 			<body>
 				<Providers>
 				        <BottomNavigation activeTab={activeTab} setActiveTab={setActiveTab} />
