de985bf6c22d58c33db6c8ff1999d048422c8d17
Myner Soft <mynersoft@gmail.com>
2025-12-13 03:36:05 +0600

    Update route.js

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,6 +1,6 @@
 import Product from "@/models/Product";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from " @/api/auth/[...nextauth]/route";
+import { authOptions } from " @/app/api/auth/[...nextauth]/route";
 import { connectDB } from "@/lib/db";
 
 export async function GET() {
