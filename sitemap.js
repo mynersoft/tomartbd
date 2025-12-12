95c7f7db83f4d0424b7627bde53d010762f958f6
Myner Soft <mynersoft@gmail.com>
2025-12-13 05:20:30 +0600

    Create sitemap.js

---
+++ sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "url",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://yourdomain.com/products",
+      lastModified: new Date(),
+    }
+  ];
+}
