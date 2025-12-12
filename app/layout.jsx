5f064d231520644ff597d2f22951d33ab08555f4
Myner Soft <mynersoft@gmail.com>
2025-12-12 22:34:58 +0600

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,10 @@
 import Providers from "./providers/Providers";
 import "./globals.css";
 import Header from "../components/Header";
+
+
+import BottomNavigation from " @/components/BottomMenu
+
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
