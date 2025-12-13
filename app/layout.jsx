47820cc84198d7712c5395caa2e0965ee697110a
mynersoft <mynersoft@gmail.com>
2025-12-13 08:55:15 +0600

    menu

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,23 +2,39 @@
 
 import Providers from "./providers/Providers";
 import "./globals.css";
-import Header from "../components/Header";
 
 
-import BottomNavigation from "../components/BottomMenu";
 
-export default function RootLayout({ children }) {
-const [activeTab, setActiveTab] = useState('Home')
+
+export default function RootLayout() {
+
+
 	return (
 		<html lang="en">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
+			<head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+			</head>
+
 			<body>
 				<Providers>
-				        <BottomNavigation activeTab={activeTab} setActiveTab={setActiveTab} />
-					{children}
+					children
 				</Providers>
 			</body>
 		</html>
--- app/providers/Providers.jsx
+++ app/providers/Providers.jsx
@@ -4,16 +4,29 @@ import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 import { store } from "@/store/store"; // <- FIXED PATH
 import { Toaster } from "react-hot-toast";
+import Header from "@/components/Header";
+import BottomNavigation from "@/components/BottomMenu";
+import { useState } from "react";
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children, session }) {
+	
+	const [activeTab, setActiveTab] = useState("Home");
+
 	return (
 		<SessionProvider session={session}>
 			<Provider store={store}>
 				<QueryClientProvider client={queryClient}>
 					<Toaster />
+					<Header />
 					{children}
+					<div className="mobile-only">
+						<BottomNavigation
+							activeTab={activeTab}
+							setActiveTab={setActiveTab}
+						/>
+					</div>
 				</QueryClientProvider>
 			</Provider>
 		</SessionProvider>
