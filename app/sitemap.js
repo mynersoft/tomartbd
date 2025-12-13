e64726fdbb1d4f1e9366272c5a06c1eb1bea5458
Myner Soft <mynersoft@gmail.com>
2025-12-13 23:29:57 +0600

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/products`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/categories`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
+}
