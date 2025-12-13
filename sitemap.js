a4591145103c2f3b12a3e6a3c76a2983462bf25a
Myner Soft <mynersoft@gmail.com>
2025-12-13 23:32:13 +0600

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,12 +1,24 @@
 export default function sitemap() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
   return [
     {
-      url: "url",
+      url: `${baseUrl}`,
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
     },
     {
-      url: "https://yourdomain.com/products",
+      url: `${baseUrl}/products`,
       lastModified: new Date(),
-    }
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/categories`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
   ];
 }
