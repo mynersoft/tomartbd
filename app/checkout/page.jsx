e224dac9f6788243b3dc5d7a9fb1bc46c06ade80
Myner Soft <mynersoft@gmail.com>
2025-12-14 18:27:15 +0600

    Delete app/checkout/page.jsx

--- app/checkout/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";  // বাধ্যতামূলক
-
-import CheckoutPageComponent from "@/components/CheckoutPageComponent";
-
-export const dynamic = "force-dynamic";
-
-export default function CheckoutPage() {
-  return <CheckoutPageComponent />;
-}
