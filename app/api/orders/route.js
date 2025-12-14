50f5102865d30087cafec6801ce5d5a0d0b80aa3
Myner Soft <mynersoft@gmail.com>
2025-12-14 22:33:35 +0600

    Update route.js

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,11 +1,12 @@
 import { connectDB } from "@/lib/db";
 import Order from "@/models/Order";
 import { getServerSession } from "next-auth";
+import {authOptions}  from " @/lib/auth";
 
 export async function POST(req) {
 	await connectDB();
 
-	const session = await getServerSession();
+	const session = await getServerSession(authOptions);
 	if (!session) {
 		return new Response("Unauthorized", { status: 401 });
 	}
