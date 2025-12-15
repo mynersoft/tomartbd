d9edd5469f12a0c2049febbc480f66e5f7ce5b21
Myner Soft <mynersoft@gmail.com>
2025-12-15 20:19:38 +0600

    Update MessengerChat.jsx

--- components/MessengerChat.jsx
+++ components/MessengerChat.jsx
@@ -1,39 +1,49 @@
 "use client";
 
-import { useEffect } from "react";
+import Script from "next/script";
 
-export default function MessengerChat({ pageId }) {
-  useEffect(() => {
-    if (document.getElementById("facebook-jssdk")) return;
+const PAGE_ID = process.env.NEXT_PUBLIC_FB_PAGE_ID;
 
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        xfbml: true,
-        version: "v19.0",
-      });
-    };
-
-    const script = document.createElement("script");
-    script.id = "facebook-jssdk";
-    script.src =
-      "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-    script.async = true;
-    script.defer = true;
-
-    document.body.appendChild(script);
-  }, []);
+export default function FacebookMessenger() {
+  if (!PAGE_ID) return null;
 
   return (
     <>
+      {/* FB root */}
       <div id="fb-root"></div>
 
+      {/* Facebook SDK */}
+      <Script
+        id="facebook-jssdk"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml: true,
+                version: 'v19.0'
+              });
+            };
+
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+          `,
+        }}
+      />
+
+      {/* Messenger chat */}
       <div
         className="fb-customerchat"
         attribution="biz_inbox"
-        page_id={pageId}
-        theme_color="#2563eb"
-        logged_in_greeting="Hi 👋 How can we help you?"
-        logged_out_greeting="Hi 👋 Please message us"
+        page_id={PAGE_ID}
+        theme_color="#0084ff"
+        logged_in_greeting="Hi! How can we help you?"
+        logged_out_greeting="Hi! Please log in to chat with us."
       ></div>
     </>
   );
