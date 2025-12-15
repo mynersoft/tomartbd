2b86efebc598a3228afcd3b7883ad0faad88071f
Myner Soft <mynersoft@gmail.com>
2025-12-15 19:27:52 +0600

    Update GoogleAnalytics.jsx

--- components/GoogleAnalytics.jsx
+++ components/GoogleAnalytics.jsx
@@ -1,27 +1,31 @@
-// components/Analytics.jsx
-import Script from 'next/script'
+'use client';
 
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+import Script from 'next/script';
 
-export default function  GoogleAnalytics = () => {
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+export default function GoogleAnalytics() {
+  if (!GA_MEASUREMENT_ID) return null;
+
   return (
     <>
+      {/* Google Analytics Script */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
       />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}');
-          `,
-        }}
-      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
     </>
-  )
+  );
 }
