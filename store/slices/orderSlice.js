b8dd3f6cc3f55d56db3a411c83866135f090a5b0
Myner Soft <mynersoft@gmail.com>
2025-12-15 08:42:12 +0600

    Update orderSlice.js

--- store/slices/orderSlice.js
+++ store/slices/orderSlice.js
@@ -1,47 +1,22 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
-
-export const placeOrderCOD = createAsyncThunk(
-	"order/placeOrderCOD",
-	async (orderData, thunkAPI) => {
-		try {
-			const res = await axios.post("/api/orders", orderData);
-			return res.data;
-		} catch (err) {
-			return thunkAPI.rejectWithValue(err.response.data);
-		}
-	}
-);
+import { createSlice } from "@reduxjs/toolkit";
 
 const orderSlice = createSlice({
-	name: "order",
-	initialState: {
-		loading: false,
-		success: false,
-		error: null,
-	},
-	reducers: {
-		resetOrder: (state) => {
-			state.loading = false;
-			state.success = false;
-			state.error = null;
-		},
-	},
-	extraReducers: (builder) => {
-		builder
-			.addCase(placeOrderCOD.pending, (state) => {
-				state.loading = true;
-			})
-			.addCase(placeOrderCOD.fulfilled, (state) => {
-				state.loading = false;
-				state.success = true;
-			})
-			.addCase(placeOrderCOD.rejected, (state, action) => {
-				state.loading = false;
-				state.error = action.payload;
-			});
-	},
+  name: "orders",
+  initialState: {
+    selectedOrder: null,
+    isModalOpen: false,
+  },
+  reducers: {
+    openOrderModal(state, action) {
+      state.selectedOrder = action.payload;
+      state.isModalOpen = true;
+    },
+    closeOrderModal(state) {
+      state.selectedOrder = null;
+      state.isModalOpen = false;
+    },
+  },
 });
 
-export const { resetOrder } = orderSlice.actions;
+export const { openOrderModal, closeOrderModal } = orderSlice.actions;
 export default orderSlice.reducer;
