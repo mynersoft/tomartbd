d20cf71cf99d93076d47a1741b0e30a92eede583
Myner Soft <mynersoft@gmail.com>
2025-12-16 15:03:33 +0600

    Update route.js

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,28 +1,53 @@
 import { connectDB } from "@/lib/db";
 import Order from "@/models/Order";
-
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
-
+/* ================= GET (Admin) ================= */
 export async function GET() {
-  await connectDB();
-  const orders = await Order.find().sort({ createdAt: -1 });
-  return Response.json(orders);
+	
+	await connectDB();
+	const orders = await Order.find().sort({ createdAt: -1 });
+	return NextResponse.json(orders);
 }
 
-
-
+/* ================= POST (User) ================= */
 export async function POST(req) {
 	const session = await getServerSession(authOptions);
 
 	if (!session) {
 		return NextResponse.json(
-			{ success: false, message: "Unauthorized" },
+			{ message: "Unauthorized" },
 			{ status: 401 }
 		);
 	}
 
-	return NextResponse.json({ success: true });
+	const body = await req.json();
+
+	await connectDB();
+
+	const order = await Order.create({
+		customer: {
+			name: session.user.name,
+			email: session.user.email,
+			phone: body.phone,
+		},
+		total: body.totalAmount,
+		payment: {
+			method: "cash_on_delivery",
+		},
+		shipping: {
+			address: body.address,
+			city: body.city,
+			phone: body.phone,
+		},
+		orderItems: body.products,
+		userId: session.user.id,
+	});
+
+	return NextResponse.json({
+		success: true,
+		order,
+	});
 }
