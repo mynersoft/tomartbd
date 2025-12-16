77863e63de6f4a9363a8735a6bfbfaa1183a9d4c
Myner Soft <mynersoft@gmail.com>
2025-12-16 14:34:25 +0600

    Update route.js

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,15 +1,28 @@
 import { connectDB } from "@/lib/db";
 import Order from "@/models/Order";
 
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+
 export async function GET() {
   await connectDB();
   const orders = await Order.find().sort({ createdAt: -1 });
   return Response.json(orders);
 }
 
+
+
 export async function POST(req) {
-  await connectDB();
-  const body = await req.json();
-  const order = await Order.create(body);
-  return Response.json(order, { status: 201 });
+	const session = await getServerSession(authOptions);
+
+	if (!session) {
+		return NextResponse.json(
+			{ success: false, message: "Unauthorized" },
+			{ status: 401 }
+		);
+	}
+
+	return NextResponse.json({ success: true });
 }
