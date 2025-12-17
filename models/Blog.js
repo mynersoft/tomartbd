c1dadbbabdcc935d1942e12b22ce13ae1cc34cb5
Myner Soft <mynersoft@gmail.com>
2025-12-17 19:22:09 +0600

    Create Blog.js

---
+++ models/Blog.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const BlogSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    content: { type: String, required: true },
+    image: String,
+    status: { type: String, default: "published" },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Blog ||
+  mongoose.model("Blog", BlogSchema);
