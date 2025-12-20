01124614754136a90d99da170aaae28c1d35412b
Myner Soft <mynersoft@gmail.com>
2025-12-20 18:50:06 +0600

    Create voucher/route.js

---
+++ app/api/voucher/route.js
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import Voucher from "@/lib/models/Voucher";
+import { connectDB } from "@/lib/db";
+import { ApiError, handleApiError } from "@/lib/api-error";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+    const body = await req.json();
+
+    if (!body.code || !body.value)
+      throw new ApiError("Required fields missing", 400);
+
+    const voucher = await Voucher.create(body);
+
+    return NextResponse.json({
+      success: true,
+      voucher,
+    });
+  } catch (error) {
+    return handleApiError(error);
+  }
+}
+
+export async function GET() {
+  try {
+    await connectDB();
+    const vouchers = await Voucher.find().sort({ createdAt: -1 });
+
+    return NextResponse.json({
+      success: true,
+      vouchers,
+    });
+  } catch (error) {
+    return handleApiError(error);
+  }
+}
