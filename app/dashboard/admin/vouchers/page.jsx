15f4d9f5b0ff53073fe5dcba740f02bd6a85dbc0
Myner Soft <mynersoft@gmail.com>
2025-12-21 08:06:23 +0600

    Update page.jsx

--- app/dashboard/admin/vouchers/page.jsx
+++ app/dashboard/admin/vouchers/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useAddVoucher } from "@/hooks/useVoucher";
+import { useAddVoucher, useVoucher } from "@/hooks/useVoucher";
 import {
 	Search,
 	Plus,
