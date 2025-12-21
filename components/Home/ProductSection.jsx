45f5e05af9df7822e50a0b20ca7535f1fb73d304
Myner Soft <mynersoft@gmail.com>
2025-12-21 15:17:27 +0600

    Create Home/ProductSection.jsx

---
+++ components/Home/ProductSection.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import ProductCard from "@/components/Product/ProductCard/";
+
+export default function ProductSection({ title, products }) {
+  if (!products || products.length === 0) return null;
+
+  return (
+    <section className="my-10">
+      <div className="flex justify-between items-center mb-6">
+        <h2 className="text-2xl font-bold">{title}</h2>
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+}
