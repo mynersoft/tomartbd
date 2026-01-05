3dae273dfb94c2639672a2197617a2b97fad2219
Myner Soft <mynersoft@gmail.com>
2026-01-05 19:52:05 +0600

    Update route.js

--- app/api/voucher/status-update/route.js
+++ app/api/voucher/status-update/route.js
@@ -1,19 +1,43 @@
-// PATCH /api/voucher/:id/status
+
 import { NextResponse } from "next/server";
 import Voucher from "@/models/Voucher";
 import { connectDB } from "@/lib/db";
+import {getIdFromReq} from " @/libgetIdFromReq";
 
-export async function PATCH(req) {
-const id = await getIdFromReq (req);
-  await connectDB();
-  
-  const { isActive } = await req.json();
+export async function PATCH(req, { params }) {
+  try {
+    const id = await getIdFromReq (req);
 
-  const voucher = await Voucher.findByIdAndUpdate(
-    id,
-    { isActive },
-    { new: true }
-  );
+    await connectDB();
 
-  return NextResponse.json({ success: true, voucher });
+    const { isActive } = await req.json();
+
+    if (!["active", "inactive"].includes(isActive)) {
+      return NextResponse.json(
+        { success: false, message: "Invalid status" },
+        { status: 400 }
+      );
+    }
+
+    const voucher = await Voucher.findByIdAndUpdate(
+      id,
+      { isActive },
+      { new: true }
+    );
+
+    if (!voucher) {
+      return NextResponse.json(
+        { success: false, message: "Voucher not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ success: true, voucher });
+  } catch (error) {
+    console.error("Voucher Status Update Error:", error);
+    return NextResponse.json(
+      { success: false, message: error.message },
+      { status: 400 }
+    );
+  }
 }
