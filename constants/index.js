72bcdf85a41b2ddacd9f94dd4dcecd56811e4a49
Myner Soft <mynersoft@gmail.com>
2026-01-17 11:09:28 +0600

    Create index.js

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+export const socailMediLinks = [
+  { name: "Facebook", Icon: Facebook, link: "#", ariaLabel: "Follow us on Facebook" },
+  { name: "Twitter", Icon: Twitter, link: "#", ariaLabel: "Follow us on Twitter" },
+  { name: "Instagram", Icon: Instagram, link: "#", ariaLabel: "Follow us on Instagram" },
+  { name: "YouTube", Icon: Youtube, link: "#", ariaLabel: "Follow us on YouTube" },
+];
