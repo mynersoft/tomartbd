8858453fa0eee946199c510e0e892437934448ac
Myner Soft <mynersoft@gmail.com>
2026-02-01 15:41:38 +0600

    Update page.jsx

--- app/vendor/messages/page.jsx
+++ app/vendor/messages/page.jsx
@@ -1,8 +1,8 @@
 'use client';
 
 import React, { useState } from 'react';
-import DashboardLayout from '../../components/vendor/DashboardLayout';
-import MessageInbox from '../../components/vendor/MessageInbox';
+import DashboardLayout from '@/components/vendor/DashboardLayout';
+import MessageInbox from '@/components/vendor/MessageInbox';
 import { 
   MessageSquare, 
   Search, 
