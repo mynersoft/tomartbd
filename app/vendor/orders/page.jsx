f57921f9009c66f20011ab8929081d3c2f2bd948
Myner Soft <mynersoft@gmail.com>
2026-02-01 17:35:43 +0600

    Update page.jsx

--- app/vendor/orders/page.jsx
+++ app/vendor/orders/page.jsx
@@ -1,8 +1,8 @@
 'use client';
 
 import React, { useState } from 'react';
-import DashboardLayout from '@/components/vendor/DashboardLayout';
-import OrderTable from '@/components/vendor/OrderTable';
+import DashboardLayout from '@/components/Vendor/DashboardLayout';
+import OrderTable from '@/components/Vendor/OrderTable';
 import { 
   Filter, 
   Download, 
